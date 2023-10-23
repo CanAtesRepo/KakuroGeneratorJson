3ad2e29904f5c8eebbea624470bb7e7e5590441c
Hafthor <git@hafthor.net>
2023-10-23 09:30:24 -0400

    nit

--- Kakuro/Program.cs
+++ Kakuro/Program.cs
@@ -1,5 +1,4 @@
-﻿if (args.Length != 2)
-{
+﻿if (args.Length != 2) {
     Console.WriteLine("Usage: Kakuro <row> <column>");
     Console.WriteLine("Example: Kakuro 20,19,8 22,18,7"); // solution: 985,792,421
     return 1;
@@ -7,7 +6,6 @@
 
 var horz = args[0].Split(',').Select(int.Parse).ToArray();
 var vert = args[1].Split(',').Select(int.Parse).ToArray();
-
 var kakuro = new Kakuro(horz, vert);
 kakuro.Solve();
 return kakuro.Print() ? 0 : 1;
